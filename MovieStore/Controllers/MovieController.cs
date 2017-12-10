f545a67f0a95fb6e6cb7ff81a6d667a7b77adc16
Tomas Lann <tomas_lann@hotmail.com>
2017-12-10 18:49:14 +0100

    Add missing files to Git after update of class

---
+++ MovieStore/Controllers/MovieController.cs
@@ -0,0 +1,27 @@
+﻿using MovieStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieStore.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Index()
+        {
+            List<MovieModel> movie = new List<MovieModel>();
+            movie.Add(new MovieModel { Titel = "Terminator", Genre = "Action", Description = "bla bla", Length = 120 });
+            movie.Add(new MovieModel { Titel = "Terminator 2", Genre = "Action", Description = "bla bla", Length = 120 });
+            movie.Add(new MovieModel { Titel = "Terminator 3", Genre = "Action", Description = "bla bla", Length = 120 });
+            movie.Add(new MovieModel { Titel = "Terminator 4", Genre = "Action", Description = "bla bla", Length = 120 });
+            movie.Add(new MovieModel { Titel = "Terminator 5", Genre = "Action", Description = "bla bla", Length = 120 });
+
+            return View(movie);
+        }
+
+
+    }
+}
---
+++ MovieStore/Models/MovieModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieStore.Models
+{
+    public class MovieModel
+    {
+        public string Titel { get; set; }
+        public int Length { get; set; }
+        public string Description { get; set; }
+        public string Genre { get; set; }
+    }
+
+
+}
