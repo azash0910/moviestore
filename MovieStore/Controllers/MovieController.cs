32654bbc602bab1e9b16d6c8525fbd5e69692d23
Tomas Lann <tomas_lann@hotmail.com>
2017-12-16 14:30:56 +0100

    Added a new database connection and CRUD for movies

--- MovieStore/Controllers/MovieController.cs
+++ MovieStore/Controllers/MovieController.cs
@@ -1,7 +1,10 @@
-﻿using MovieStore.Models;
+﻿using MovieStore.Context;
+using MovieStore.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,19 +12,117 @@ namespace MovieStore.Controllers
 {
     public class MovieController : Controller
     {
+        MovieContext db = new MovieContext();
+
         // GET: Movie
         public ActionResult Index()
         {
-            List<MovieModel> movie = new List<MovieModel>();
-            movie.Add(new MovieModel { Titel = "Terminator", Genre = "Action", Description = "bla bla", Length = 120 });
-            movie.Add(new MovieModel { Titel = "Terminator 2", Genre = "Action", Description = "bla bla", Length = 120 });
-            movie.Add(new MovieModel { Titel = "Terminator 3", Genre = "Action", Description = "bla bla", Length = 120 });
-            movie.Add(new MovieModel { Titel = "Terminator 4", Genre = "Action", Description = "bla bla", Length = 120 });
-            movie.Add(new MovieModel { Titel = "Terminator 5", Genre = "Action", Description = "bla bla", Length = 120 });
+            var movies = db.Movies.ToList();
 
+            return View(movies);
+        }
+
+        // GET: Movie/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            MovieModel movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
             return View(movie);
         }
 
+        // GET: Movie/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
 
+        // POST: Movie/Create
+        [HttpPost]
+        public ActionResult Create(MovieModel movie)
+        {
+            try
+            {
+                db.Movies.Add(movie);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Movie/Edit/5
+        public ActionResult Edit(int id)
+        {
+            MovieModel movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: Movie/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, MovieModel movie)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    db.Entry(movie).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Movie/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MovieModel movie = db.Movies.Find(id);
+
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: Movie/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                MovieModel movie = db.Movies.Find(id);
+                db.Movies.Remove(movie);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
     }
 }
--- MovieStore/Models/MovieModel.cs
+++ MovieStore/Models/MovieModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,8 @@ namespace MovieStore.Models
 {
     public class MovieModel
     {
+        [Key]
+        public int Id { get; set; }
         public string Titel { get; set; }
         public int Length { get; set; }
         public string Description { get; set; }
